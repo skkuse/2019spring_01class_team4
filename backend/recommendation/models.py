a84d740d234bd79b7db3fc7ca5409fdf517541c9
csy1204 <josang1204@gmail.com>
2019-06-07 11:06:42 +0900

    히스토리 모델 설명 추가

--- backend/recommendation/models.py
+++ backend/recommendation/models.py
@@ -8,6 +8,6 @@ from problem.models import ProblemEX, Problem
 # 추천 히스토리 
 # 1유저 - 1문제 추천 당 하나의 히스토리 생성
 # 문제, 유저 외래키 생성
-# round (int), created_at (datetime,생성될 때 자동으로 값 넣게끔), difficulty
+# round (int), is_Ex(boolean) created_at (datetime,생성될 때 자동으로 값 넣게끔), difficulty
 class RecommendHistory(models.Model):
     pass
