a8e12dde8fa07fcebccf94487e923bf9855b61f4
csy1204 <josang1204@gmail.com>
2019-06-09 13:43:24 +0900

    추천기록 업데이트 / 추천 고도화 / User 현재 추천

--- backend/recommendation/models.py
+++ backend/recommendation/models.py
@@ -10,20 +10,19 @@ from problem.models import ProblemEX, Problem
 # 문제, 유저 외래키 생성
 # round (int), is_Ex(boolean) created_at (datetime,생성될 때 자동으로 값 넣게끔), difficulty
 class RecommendHistory(models.Model):
-    round=models.IntegerField()
-    is_Ex=models.BooleanField()
-    is_Solved=models.BooleanField() #풀이여부
-    created_at=models.DataTimeField(auto_now_add=True, null=True)
+    round_num=models.IntegerField()
+    is_Ex=models.BooleanField(default=False)
+    is_Solved=models.BooleanField(default=False) #풀이여부
 
-    difficulty=models.ForeignKey('problem.QuriousDifficulty', null=True, related_name='difficulty', on_delete=models.CASCADE)
-    user=models.ForeignKey(User, related_name= "users", on_delete=models.CASCADE)
-    problem=models.ForeignKey(Problem, related_name= "problems", on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True, null=True)
+
+    difficulty=models.ForeignKey('problem.QuriousDifficulty', null=True, related_name='recommendhistory', on_delete=models.CASCADE)
+
+    user=models.ForeignKey('account.User', related_name= "recommendhistory", on_delete=models.CASCADE)
+    problemex=models.ForeignKey('problem.ProblemEX', null=True, related_name= "recommendhistory", on_delete=models.CASCADE)
+    problem=models.ForeignKey('problem.Problem', null=True, related_name= "recommendhistory", on_delete=models.CASCADE)
 
     class Meta:
         db_table="recommend_history"
         ordering=("-created_at",)
-'''
-    def add_round(self): #필요?
-        self.round = models.F("round") + 1
-        self.save(update_fields=["round"])
-'''
+
